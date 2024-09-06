407609d60b02d4bfdd30b13512d7901bfa7a6479
Joseph Noblett <noblettjoe@gmail.com>
2024-09-06 05:18:33 -0400

    router, migration index package.json complete. WIP models

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,22 @@
-// build your `/api/projects` router here
+const express = require('express');
+const helpers = require('./model');
+
+const router = express.Router();
+
+router.get('/projects', (req, res, next) => {
+  helpers.get_projects()
+    .then(project => {
+      res.status(200).json(project);
+    })
+    .catch(next); 
+});
+
+router.post('/projects', (req, res, next) => {
+  helpers.create_project(req.body)
+    .then(project => {
+      res.status(201).json(project);
+    })
+    .catch(next);
+});
+
+module.exports = router
