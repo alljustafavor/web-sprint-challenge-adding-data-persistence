1eaf1e81f778fc5499824a2b80f9d9d457dd62bd
Joseph Noblett <noblettjoe@gmail.com>
2024-09-06 11:50:41 -0400

    MVP complete

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,26 @@
-// build your `Project` model here
+const db = require('../../data/dbConfig');
+
+const getProjects = () => {
+  return db('projects')
+    .select('project_id', 'project_name', 'project_description', 'project_completed')
+    .then(projects => projects.map(project => ({
+      ...project,
+      project_completed: Boolean(project.project_completed)
+    })));
+};
+
+const addProject = async (project) => {
+  const [id] = await db('projects').insert(project);
+  return db('projects')
+    .where('project_id', id)
+    .first()
+    .then(project => ({
+      ...project,
+      project_completed: Boolean(project.project_completed)
+    }));
+};
+
+module.exports = {
+  getProjects,
+  addProject
+}
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,22 +1,30 @@
 const express = require('express');
-const helpers = require('./model');
+const Resource = require('./model');
 
 const router = express.Router();
 
-router.get('/resources', (req, res, next) => {
-  helpers.get_resources(req.params.id)
-    .then(resource => {
-      res.status(200).json(resource);
-    })
-    .catch(next);
+router.get('/', async (req, res, next) => {
+  try {
+    const resources = await Resource.getResources();
+    res.json(resources);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.post('/resources', (req, res, next) => {
-  helpers.create_resource(req.body)
-    .then(resource => {
-      res.status(201).json(resource);
-    })
-    .catch(next);
+router.post('/', async (req, res, next) => {
+  try {
+    if (!req.body.resource_name) {
+      return res.status(400).json({ message: "resource_name is required" });
+    }
+    const newResource = await Resource.addResource(req.body);
+    res.status(201).json(newResource);
+  } catch (err) {
+    if (err.code === 'SQLITE_CONSTRAINT') {
+      return res.status(400).json({ message: "resource_name must be unique" });
+    }
+    next(err);
+  }
 });
 
-module.exports = router
+module.exports = router;
